41e6568c515127ad73d6be1324c10f7d83d4b2af
nguyen <nguyen.nc2710@gmail.com>
2023-07-04 12:24:11 +0700

    add basic lsp web dev languages

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,11 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "tsserver",
+        "eslint",
+        "html",
+        "cssls",
       })
     end,
   },
@@ -19,7 +23,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "prettier",
-        -- "stylua",
+        "stylua",
       })
     end,
   },
