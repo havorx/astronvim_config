cd5d83f12db1449c5249ea45b19f8e2dea891f1b
nguyen <nguyen.nc2710@gmail.com>
2023-07-31 21:57:20 +0700

    colorscheme: change to carbonfox theme

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,8 +3,10 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
+  -- theme
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.code-runner.overseer-nvim" },
 }
