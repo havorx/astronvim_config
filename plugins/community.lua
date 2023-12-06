72cb9ef58e9294d8801051c4a8895bea54a2549f
nguyen <nguyen.nc2710@gmail.com>
2023-12-07 03:33:10 +0700

    plugin: adjust community plugin, add dad-bod, docker, vim-move

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,10 +4,14 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   -- theme
-  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.colorscheme.nightfox-nvim" },
 
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.code-runner.overseer-nvim" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
   { import = "astrocommunity.workflow.hardtime-nvim" },
+  { import = "astrocommunity.pack.full-dadbod" },
+  { import = "astrocommunity.pack.docker" },
+
+  -- { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.editing-support.vim-move" },
 }
