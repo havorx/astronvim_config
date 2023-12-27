22a7ea2571399b1756da6db15eb4c1eb72112da3
nguyen <nguyen.nc2710@gmail.com>
2023-12-28 03:23:03 +0700

    config: temporarily disable typescript.nvim

--- plugins/typescript/init.lua
+++ plugins/typescript/init.lua
@@ -117,7 +117,7 @@ return {
     opts = {},
     event = "BufRead package.json",
   },
-  {
+  --[[ {
     "jose-elias-alvarez/typescript.nvim",
     init = function() astronvim.lsp.skip_setup = utils.list_insert_unique(astronvim.lsp.skip_setup, "tsserver") end,
     ft = {
@@ -133,7 +133,7 @@ return {
     opts = function(_, opts)
       opts.sources = utils.list_insert_unique(opts.sources, require "typescript.extensions.null-ls.code-actions")
     end,
-  },
+  }, ]]
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts)
