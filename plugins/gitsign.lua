986ee96cb5c92b2158db6ed429aef58f67caf69b
nguyen <nguyen.nc2710@gmail.com>
2023-12-28 03:23:47 +0700

    config: reconfig gitsign and status column to be narrower

--- plugins/gitsign.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "lewis6991/gitsigns.nvim",
-  opts = function(_, opts)
-    opts.signcolumn = false
-    opts.numhl = true
-  end,
-}
--- plugins/statuscolumn.lua
+++ plugins/statuscolumn.lua
@@ -3,18 +3,15 @@ return {
   opts = function(_, opts)
     local status = require "astronvim.utils.status"
 
-    local signcolumn = {
-      padding = { right = 0, left = 0 },
-    }
-
     opts.statuscolumn = vim.fn.has "nvim-0.9" == 1
         and {
           init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
-          status.component.signcolumn(signcolumn),
           status.component.numbercolumn(),
+          status.component.signcolumn(),
         }
       or nil
 
-    vim.o.signcolumn = "number"
+    vim.o.statuscolumn = "%=%{v:relnum?v:relnum:v:lnum} %s"
+    vim.o.signcolumn = "yes"
   end,
 }
